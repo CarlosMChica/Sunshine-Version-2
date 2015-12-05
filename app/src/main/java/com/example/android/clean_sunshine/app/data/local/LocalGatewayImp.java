dd9ba484ab2122b35fedca3f3f3a883af6fe7d5c
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2015-12-05 18:32:03 +0000

    remove static import

--- app/src/main/java/com/example/android/clean_sunshine/app/data/local/LocalGatewayImp.java
+++ app/src/main/java/com/example/android/clean_sunshine/app/data/local/LocalGatewayImp.java
@@ -4,6 +4,7 @@ import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import com.example.android.clean_sunshine.app.data.local.ForecastContract.WeatherEntry;
 import com.example.android.clean_sunshine.app.domain.model.Forecast;
 import com.example.android.clean_sunshine.app.domain.model.LocalGateway;
 import java.util.ArrayList;
@@ -11,8 +12,6 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-import static com.example.android.clean_sunshine.app.data.local.ForecastContract.WeatherEntry.COLUMN_DATE;
-import static com.example.android.clean_sunshine.app.data.local.ForecastContract.WeatherEntry.CONTENT_URI;
 import static java.lang.String.valueOf;
 import static java.util.Calendar.DAY_OF_YEAR;
 import static java.util.Calendar.getInstance;
@@ -32,7 +31,7 @@ public class LocalGatewayImp implements LocalGateway {
 
   @Override public List<Forecast> load() {
     List<Forecast> forecastList = new ArrayList<>();
-    Cursor cursor = contentResolver.query(CONTENT_URI, null, null, null, null);
+    Cursor cursor = contentResolver.query(WeatherEntry.CONTENT_URI, null, null, null, null);
     if (cursor != null) {
       while (cursor.moveToNext()) {
         forecastList.add(mapper.mapFromDb(cursor));
@@ -44,15 +43,18 @@ public class LocalGatewayImp implements LocalGateway {
   @Override public void update(List<Forecast> forecastList) {
     List<ContentValues> contentValues = mapper.mapToDb(forecastList);
     ContentValues[] values = contentValues.toArray(new ContentValues[contentValues.size()]);
-    contentResolver.bulkInsert(CONTENT_URI, values);
+    contentResolver.bulkInsert(WeatherEntry.CONTENT_URI, values);
     purge();
+  }
+
+  private void location(){
   }
 
   private void purge() {
     Calendar calendar = getInstance();
     calendar.add(DAY_OF_YEAR, -1);
     Date yesterday = calendar.getTime();
-    contentResolver.delete(CONTENT_URI, COLUMN_DATE + " <= ?",
+    contentResolver.delete(WeatherEntry.CONTENT_URI, WeatherEntry.COLUMN_DATE + " <= ?",
         new String[] {valueOf(yesterday.getTime())});
   }
 }
